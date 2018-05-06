f40274a6274ea5e29e6734e83a009e2e77af6d0c
Juan Alonso <juan@neuronasmuertas.com>
2018-05-06 21:58:36 +0200

    Añadidos seis potenciómetros para controlar los principales parámetros de la lámpara

--- fastlamp.ino
+++ fastlamp.ino
@@ -3,21 +3,26 @@
 #define NUM_LEDS 24
 #define DATA_PIN  7
 
-#define RADIUS 1.8
+enum potType {
+  ROTATION, HUE, Z, RADIUS, BRIGHTNESS, SATURATION
+};
 
 CRGB leds[NUM_LEDS];
 
 uint32_t cx, cy, cz;
 
+int analogValue[6];
+
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   LEDS.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  //LEDS.setBrightness(150);
+  LEDS.setBrightness(0);
 
   random16_set_seed(8934);
-  random16_add_entropy(analogRead(3));
+  //random16_add_entropy(analogRead(3));
+
 
   //cX van de 0 a 65k
   cx = random16();
@@ -29,21 +34,29 @@ void setup() {
 
 void loop() {
 
+  for (int f = 0; f < 6; f++) {
+    analogValue[f] = 1023 - analogRead(f);
+  }
+
   //rotation va de 0 a 65k
   static uint16_t rotation = 0;
 
   static uint8_t hue = 0;
 
   EVERY_N_MILLISECONDS(25) {
-    rotation += 55;
+    rotation += map(analogValue[ROTATION], 0, 1000, 0, 1750);
   }
 
-  EVERY_N_MILLISECONDS(110) {
-    hue++;
+  EVERY_N_MILLISECONDS(100) {
+    hue += map(analogValue[HUE], 0, 1000, 0, 5);
   }
 
   EVERY_N_MILLISECONDS(50) {
-    cz += 80;
+    cz += map(analogValue[Z], 0, 1000, 0, 2500);
+  }
+
+  EVERY_N_MILLISECONDS(100) {
+    LEDS.setBrightness(map(analogValue[BRIGHTNESS], 0, 1020, 5, 250));
   }
 
   for (int pixel = 0; pixel < NUM_LEDS; pixel++) {
@@ -58,14 +71,14 @@ void loop() {
     //OJO: para que la escala sea correcta, RADIUS siempre ha de llevar punto decimal
     //No tengo muy claro por qué si pongo uint en vez de int,
     //x a veces genera una onda cuadrada
-    int32_t x = cx + cos16(ledAngle) * RADIUS;
-    int32_t y = cy + sin16(ledAngle) * RADIUS;
+    int32_t x = cx + cos16(ledAngle) * map(analogValue[RADIUS], 0, 1000, 0, 500) / 100.0;
+    int32_t y = cy + sin16(ledAngle) * map(analogValue[RADIUS], 0, 1000, 0, 500) / 100.0;
 
     //noise va de 0 a 0xFFFF, así que lo ajustamos a un uint_8
     uint8_t noiseH = inoise16(x, y, cz) >> 8;
     uint8_t noiseV = inoise16(y, x, cz) >> 8;
 
-    leds[pixel] = CHSV(noiseH + hue, 255, noiseV);
+    leds[pixel] = CHSV(noiseH + hue, map(analogValue[SATURATION],0,1000,0,250), 255);
 
   }
 
